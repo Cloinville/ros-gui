5634eb471fe2cbcdd61dd6f57074cdb1b95a903d
cloinville <colin.hinton700@myci.csuci.edu>
2020-02-17 21:16:58 -0800

    Adding Local Files to repo

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /opt/ros/kinetic/share/rqt_mypkg
+# Build directory: /opt/ros/kinetic/share/rqt_mypkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
