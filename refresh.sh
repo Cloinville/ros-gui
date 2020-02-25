4a20106cbff3524ec5f8f97be354231c1a8ae476
chinton <chinton@gblsys.com>
2020-02-25 13:54:36 -0800

    Added refresh script to help deployment and testing

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -rf /opt/ros/kinetic/share/rqt_mypkg
+#Change if package name changes
+sudo cp -r ./../ros-gui/ /opt/ros/kinetic/share/
+sudo mv /opt/ros/kinetic/share/ros-gui /opt/ros/kinetic/share/rqt_mypkg
+
