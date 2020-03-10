b32c6eb5cccf0c9d60efffac46b39edc1c5b6c6e
Colin Hinton <colin.hinton700@myci.csuci.edu>
2020-03-10 07:09:53 -0700

    deploy folder now portable and ssh works

--- refresh.sh
+++ refresh.sh
@@ -4,4 +4,6 @@ sudo rm -rf /opt/ros/kinetic/share/rqt_mypkg
 #Change if package name changes
 sudo cp -r ./../ros-gui/ /opt/ros/kinetic/share/
 sudo mv /opt/ros/kinetic/share/ros-gui /opt/ros/kinetic/share/rqt_mypkg
+sudo cp -r ./deploy /home/colin/
+sudo chmod 777 /home/colin/deploy/*
 
